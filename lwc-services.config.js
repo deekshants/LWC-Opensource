ceaa068468dc51e4f2b58c1f46f05c13a9a6780f
deekshants <deekshant.sharma@athenalogics.com>
2019-11-01 13:40:23 +0530

    .

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,7 +1,7 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/master/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    //resources: [{ from: 'src/client/resources', to: 'dist/resources' }],
+    resources: [{ from: 'src/client/resources', to: 'dist/resources' }],
     sourceDir: './src/client',
     moduleDir: './src/client/modules',
     server: {
