a47c0b1419b4c2254ebd37bcf4677984c50c92ee
deekshants <deekshant.sharma@athenalogics.com>
2019-11-04 12:54:29 +0530

    account API call

--- src/client/modules/my/compositionIteration/compositionIteration.js
+++ src/client/modules/my/compositionIteration/compositionIteration.js
@@ -1,22 +1,22 @@
 import { track } from 'lwc';
-import LightningElementSLDS from '../../../utils/LightningElementSLDS'
+import LightningElementSLDS from '../../../utils/LightningElementSLDS';
 export default class CompositionIteration extends LightningElementSLDS {
     @track accounts = [];
     error;
-    connectedCallback(){
+    connectedCallback() {
         fetch('/accounts')
-        .then(response => {
-            if (!response.ok) {
-                this.error = response;
-            }
-            return response.json()
-        })
-        .then(jsonResponse => {
-            this.accounts = jsonResponse;
-            console.log(this.accounts);
-        })
-        .catch(err => {
-            this.error = err;
-        });
+            .then(response => {
+                if (!response.ok) {
+                    this.error = response;
+                }
+                return response.json();
+            })
+            .then(jsonResponse => {
+                this.accounts = jsonResponse;
+                console.log(this.accounts);
+            })
+            .catch(err => {
+                this.error = err;
+            });
     }
 }
